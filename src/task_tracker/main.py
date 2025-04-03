4e3967a3bb229e876310a059aa1069673c839638
Max Klyuchnikov <me.klyuchnikov@gmail.com>
2025-04-03 12:49:57 +0300

    add get_tasks endpoint

--- src/task_tracker/main.py
+++ src/task_tracker/main.py
@@ -4,6 +4,19 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get('/')
-def root():
-    return {'message': 'Hello!'}
+tasks = [
+    {
+        "id": 1,
+        "title": "Написать код для такст-трекера",
+        "status": "в работе"
+    },
+    {
+        "id": 2,
+        "title": "Протестировать API",
+        "status": "не начато"
+    }
+]
+
+@app.get('/tasks')
+def get_tasks():
+    return tasks
