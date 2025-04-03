d150ca18f320180ad3b7b4dc526c8e41c8ca07bd
Max Klyuchnikov <me.klyuchnikov@gmail.com>
2025-04-03 13:08:16 +0300

    add update_task endpoint

--- src/task_tracker/main.py
+++ src/task_tracker/main.py
@@ -5,23 +5,26 @@ app = FastAPI()
 
 
 tasks = [
-    {
-        "id": 1,
-        "title": "Написать код для такст-трекера",
-        "status": "в работе"
-    },
-    {
-        "id": 2,
-        "title": "Протестировать API",
-        "status": "не начато"
-    }
+    {"id": 1, "title": "Написать код для такст-трекера", "status": "в работе"},
+    {"id": 2, "title": "Протестировать API", "status": "не начато"},
 ]
 
-@app.get('/tasks')
+
+@app.get("/tasks")
 def get_tasks():
     return tasks
 
-@app.post('/task')
-def add_task(new_task: dict):
+
+@app.post("/tasks")
+def create_task(new_task: dict):
     tasks.append(new_task)
-    return {'message': 'Задача успешно добавлена'}
+    return {"message": "Задача успешно добавлена"}
+
+
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int, updated_task: dict):
+    for task in tasks:
+        if task["id"] == task_id:
+            task.update(updated_task)
+            return {"message": f"Задача № {task_id} успешно обновлена"}
+    return {"error": f"Задача с № {task_id} не найдена"}
