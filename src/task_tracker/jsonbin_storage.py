70e83e1d1150c81d935d03acb477db253acb0935
Max Klyuchnikov <me.klyuchnikov@gmail.com>
2025-04-05 21:44:48 +0300

    edit json_storage.py to wokr with dase absctract class

--- src/task_tracker/jsonbin_storage.py
+++ src/task_tracker/jsonbin_storage.py
@@ -1,52 +1,45 @@
 import os
 
-import requests
 from dotenv import load_dotenv
+
+from .base_http_client import BaseHTTPClient
 
 load_dotenv()
 
 
-class JSONBinStorage:
+class JSONBinStorage(BaseHTTPClient):
     def __init__(self):
+        super.__init__()
         self.master_key = os.getenv("X_Master_Key")
         self.access_key = os.getenv("X_Access_Key")
         self.bin_id = None
-        self.base_url = "https://api.jsonbin.io/v3/b"
+
+    def get_headers(self):
+        return {
+            "Content-Type": "application/json",
+            "X-Master-Key": self.master_key,
+            "X-Access-Key": self.access_key,
+        }
+
+    def get_base_url(self):
+        return "https://api.jsonbin.io/v3/b"
 
     def save_tasks(self, tasks):
         """Сохраняет задачи, создаёт bin при первом вызове."""
         if not self.bin_id:
-            response = requests.post(
-                self.base_url,
-                json=tasks,
-                headers={
-                    "Content-Type": "application/json",
-                    "X-Master-Key": self.master_key,
-                    "X-Access-Key": self.access_key,
-                },
-            )
+            response = self._send_request("POST", self.get_base_url(), json=tasks)
             self.bin_id = response.json()["metadata"]["id"]
         else:
-            requests.put(
-                f"{self.base_url}/{self.bin_id}",
-                json=tasks,
-                headers={
-                    "Content-Type": "application/json",
-                    "X-Master-Key": self.master_key,
-                    "X-Access-Key": self.access_key,
-                },
+            self._send_request(
+                "PUT", f"{self.get_base_url()}/{self.bin_id}", json=tasks
             )
-        
+
     def load_tasks(self):
-        '''Загружает задачи из bin'''
+        """Загружает задачи из bin"""
         if not self.bin_id:
             return []
-        
-        response = requests.get(
-                f'{self.base_url}/{self.bin_id}/latest',
-                headers={
-                    "X-Master-Key": self.master_key,
-                    "X-Access-Key": self.access_key,
-                },
-            )
+
+        response = self._send_request(
+            "GET", f"{self.get_base_url()}/{self.bin_id}/latest"
+        )
         return response.json().get("record", [])
