2714c5141da94131e9460a7e571e561b6e401352
Max Klyuchnikov <me.klyuchnikov@gmail.com>
2025-04-06 21:10:01 +0300

    edit base_http_client.py

--- src/task_tracker/base_http_client.py
+++ src/task_tracker/base_http_client.py
@@ -1,19 +1,10 @@
 from abc import ABC, abstractmethod
 
-import requests
-
 
 class BaseHTTPClient(ABC):
-    def __init__(self):
-        self.base_url = self.get_base_url()
-        self.headers = self.get_headers()
-
-    def _send_request(self, method, url, data=None, json=None):
-        response = requests.request(
-            method, url, headers=self.headers, data=data, json=json
-        )
-        response.raise_for_status()
-        return response
+    @abstractmethod
+    def send_request(self):
+        pass
 
     @abstractmethod
     def get_headers(self):
