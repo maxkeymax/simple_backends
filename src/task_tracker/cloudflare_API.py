8af3b9b9757a2ed28b69d62a9a7934358de6862d
Max Klyuchnikov <me.klyuchnikov@gmail.com>
2025-04-07 12:01:45 +0300

    edit cloudflare_API.py and jsonbin_storage.py

--- src/task_tracker/cloudflare_API.py
+++ src/task_tracker/cloudflare_API.py
@@ -1,40 +1,51 @@
-import os
-
-from dotenv import load_dotenv
+import requests
 
 from .base_http_client import BaseHTTPClient
-
-load_dotenv()
+from .config import ConfigAPI
 
 
 class CloudflareAPI(BaseHTTPClient):
     def __init__(self):
-        super().__init__()
-        self.model = "@cf/meta/llama-3-8b-instruct"
-        self.cloudflare_api_token = os.getenv("Cloudflare_API_Token")
-        self.inputs = [
-            {
-                "role": "system",
-                "content": "Твоя роль - личный ассистент, который советует мне как выполнить поставленную задачу",
-            },
-            {
-                "role": "user",
-                "content": "Напиши короткую в 2 или 3 предложения рекомендацию по выполнению ниже приведенной задачи. Ответы пиши на русском языке",
-            },
-        ]
+        self.model = ConfigAPI.CLOUDFLARE_MODEL
+        self.cloudflare_api_token = ConfigAPI.CLOUDFLARE_API_TOKEN
+        self.cloudflare_acc_id = ConfigAPI.CLOUDFLARE_ACCOUND_ID
+        self.inputs = ConfigAPI.INPUTS
 
     def get_headers(self):
-        return {"Authorization": f"Bearer {os.getenv('Cloudflare_API_Token')}"}
+        return {"Authorization": f"Bearer {self.cloudflare_api_token}"}
 
     def get_base_url(self):
-        return f"https://api.cloudflare.com/client/v4/accounts/{os.getenv('Cloudflare_Account_ID')}/ai/run/"
+        return f"https://api.cloudflare.com/client/v4/accounts/{self.cloudflare_acc_id}/ai/run/"
 
-    def get_llm_answer(self, task):
-        user_input_task = {"role": "user", "content": task}
-        if len(self.inputs) > 2:
-            self.inputs = self.inputs[:2]
-        self.inputs.append(user_input_task)
-        response = self._send_request(
-            "POST", f"{self.get_base_url()}{self.model}", json={"messages": self.inputs}
-        )
+    def send_request(self, task):
+        inputs = self.inputs.copy()
+        inputs.append({"role": "user", "content": task})
+
+        url = f"{self.get_base_url()}{self.model}"
+        headers = self.get_headers()
+        msg_to_llm = {"messages": inputs}
+
+        response = requests.post(url, headers=headers, json=msg_to_llm)
+
         return response.json()["result"]["response"]
+
+
+
+
+# def send_request(self, task):
+#     user_input_task = {"role": "user", "content": task}
+#     if len(self.inputs) > 2:
+#         self.inputs = self.inputs[:2]
+#     self.inputs.append(user_input_task)
+#     response = self._send_request(
+#         "POST", f"{self.get_base_url()}{self.model}", json={"messages": self.inputs}
+#     )
+#     return response.json()["result"]["response"]
+
+
+# def send_request(self, method, url, data=None, json=None):
+#     response = requests.request(
+#         method, url, headers=self.headers, data=data, json=json
+#     )
+#     response.raise_for_status()
+#     return response
--- src/task_tracker/jsonbin_storage.py
+++ src/task_tracker/jsonbin_storage.py
@@ -1,43 +1,44 @@
-import os
-
-from dotenv import load_dotenv
+import requests
 
 from .base_http_client import BaseHTTPClient
-
-load_dotenv()
+from .config import ConfigJSONBin
 
 
 class JSONBinStorage(BaseHTTPClient):
     def __init__(self):
-        super().__init__()
         self.bin_id = None
 
     def get_headers(self):
         return {
             "Content-Type": "application/json",
-            "X-Master-Key": os.getenv("X_Master_Key"),
-            "X-Access-Key": os.getenv("X_Access_Key"),
+            "X-Master-Key": ConfigJSONBin.MASTER_KEY,
+            "X-Access-Key": ConfigJSONBin.ACCESS_KEY,
         }
 
     def get_base_url(self):
         return "https://api.jsonbin.io/v3/b"
 
-    def save_tasks(self, tasks):
-        """Сохраняет задачи, создаёт bin при первом вызове."""
-        if not self.bin_id:
-            response = self._send_request("POST", self.get_base_url(), json=tasks)
-            self.bin_id = response.json()["metadata"]["id"]
-        else:
-            self._send_request(
-                "PUT", f"{self.get_base_url()}/{self.bin_id}", json=tasks
-            )
-
-    def load_tasks(self):
+    def send_request(self):
         """Загружает задачи из bin"""
         if not self.bin_id:
             return []
 
-        response = self._send_request(
-            "GET", f"{self.get_base_url()}/{self.bin_id}/latest"
-        )
+        url = f"{self.get_base_url()}/{self.bin_id}/latest"
+        headers = self.get_headers()
+
+        response = requests.post(url, headers=headers)
         return response.json().get("record", [])
+
+    def save_tasks(self, tasks):
+        """Сохраняет задачи, создаёт bin при первом вызове"""
+        url = self.get_base_url()
+        headers = self.get_headers()
+
+        if not self.bin_id:
+            response = requests.post(url, headers=headers, json=tasks)
+            self.bin_id = response.json()["metadata"]["id"]
+        else:
+            url = f"{self.get_base_url()}/{self.bin_id}"
+            response = requests.put(url, headers=headers, json=tasks)
+        return response.json().get("record", [])
+            
